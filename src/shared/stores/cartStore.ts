17a5c8c9a5614dc5dcade8120d26528a0c20f1b6
Marlon Lavanda <marlonlavanda@hotmail.com>
2025-03-09 21:00:57 +0000

    feature(cart): added persistant to the cart store

--- src/shared/stores/cartStore.ts
+++ src/shared/stores/cartStore.ts
@@ -1,12 +1,30 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
 import { Product } from "../../features/products/types/Product";
 
 interface CartState {
   items: Product[];
   addItem: (product: Product) => void;
+  removeItem: (id: string) => void;
+  clearCart: () => void;
 }
 
-export const useCartStore = create<CartState>((set) => ({
-  items: [],
-  addItem: (product) => set((state) => ({ items: [...state.items, product] })),
-}));
+export const useCartStore = create<CartState>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => ({ items: [...state.items, product] })),
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.id !== id),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage", // Key in localStorage
+      storage: createJSONStorage(() => localStorage), // Explicitly use localStorage
+    }
+  )
+);
